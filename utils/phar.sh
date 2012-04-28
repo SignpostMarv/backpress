0e8d1cd9b7e3400d2c40516ef888eacc8add1ee1
SignpostMarv <github@signpostmarv.name>
2012-04-28 23:22:21 +0100

    appending same standalone 7-zip script used by libAurora.php

--- utils/phar.sh
+++ utils/phar.sh
@@ -3,3 +3,13 @@ cd ${DIR}
 mkdir -p ../build/
 rm -f ../build/*.phar ../build/*.phar.gz
 php -f ${DIR}/phar.php
+if [ -f ../../7za.exe ];
+then
+	for i in ../build/*.phar;
+	do
+		if [ -f ${i}.gz ];
+			then rm -f ${i}.gz;
+		fi;
+		../../7za.exe a -tgzip ${i}.gz ${i} -mx=9 -mfb=258 -mpass=15 ;
+	done;
+fi;
